a027024362b837547ffca6e3daed6f2ace6898bd
Lock19 <ivanksrj@gmail.com>
2023-07-07 21:39:57 -0600

    Actualizacion Ivan

--- includes/pie.php
+++ includes/pie.php
@@ -4,69 +4,71 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/js/bootstrap.bundle.min.js"></script>
-    <link href="includes\estilos1.css" rel="stylesheet" type="text/css">
+    <link href="includes\styles.css" rel="stylesheet" type="text/css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 	  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 	  <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <title>IDEA</title>
 </head>
 <body>
-<div class="container-fluid" id="Pie">
-  <div class="row">
-    <div class="col-lg-4 mb-5 mb-lg-0">
-      <h2 class="Tpie">Servicios</h2>
-      <ul class="lista" style="list-style: none;">
-        <li>Desarrollo Web</li>
-        <li>Desarrollo App Web</li>
-        <li>Diseño Web Profesional</li>
-        <li>Diseño de Tiendas</li>
-        <li>Diseño de Tiendas Online</li>
-      </ul>
-    </div>
-    <div class="col-lg-4 mb-5 mb-lg-0">
-    <img src="img/Logo Oficial.png" style="width: 230px; left: 60px; position: relative; top: 50px;">
-					<h6 class="lista2">Agencia de diseño, desarrollo</h6>
-					<h6 class="lista2" style="left: 130px;">y programación web</h6>
-					<h6 class="lista2" style="top: 70px;">¡Siguenos!</h6>
-          <br>
-          <br>
-          <br>
-          <div class="row" id="espacio">
-          <div class="d-flex justify-content-between flex-wrap">
-        <button class="btn btn-primary rounded-circle btn-circle m-2" style="background-color: #FFFFFF">
-          <a href="https://es-la.facebook.com/">
-          <img src="img\facebook.png" alt="Botón 1" width="20" height="20" style="left=30px">
-        </button></a>
-        <a href="https://www.instagram.com/">
-        <button class="btn btn-primary rounded-circle btn-circle m-2" style="background-color: #FFFFFF">
-          <img src="img\instagram.png" alt="Botón 2" width="20" height="20">
-        </button></a>
-        <a href="https://twitter.com/?lang=es">
-        <button class="btn btn-primary rounded-circle btn-circle m-2" style="background-color: #FFFFFF">
-          <img src="img\gorjeo.png" alt="Botón 3" width="20" height="20">
-        </button></a>
-        <a href="https://mx.linkedin.com/">
-        <button class="btn btn-primary rounded-circle btn-circle m-2" style="background-color: #FFFFFF">
-          <img src="img\linkedin.png" alt="Botón 4" width="20" height="20">
-        </button></a>
-      </div>
-          </div>
-    </div>
-    <div class="col-lg-4 mb-5 mb-lg-0">
-    <h2 class="Tpiec">Contacto</h2>
-      <ul class="listac" style="list-style: none;">
-        <li>ejemplo@gmail.com</li>
-        <li>+52 2221524875</li>
-        <li>CDMX, COL. Benito Juárez</li>
-        <li>90700, Ciudad de México</li>
-      </ul>
-    </div>
-  </div>
-</div>
-
-<div class="container-fluid" id="copyright">
-	Copyright© 2023 IDEA SOLUTIONS | todos los derechos reservados
-</div>
+<div class="footer text-center"> 
+<div class="container" >
+                <div class="row">
+                    <!-- Footer Location-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h2 class="text-uppercase mb-4" id="Tpie">Servicios</h2>
+                        <p id="Tpie1">
+                            Desarrollo Web
+                            <br />
+                            Desarrollo App Web
+                            <br>
+                            Diseño Web Profesional
+                            <br>
+                            Diseño de Tiendas
+                            <br>
+                            Diseño de Tiendas Online
+                        </p>
+                    </div>
+                    <!-- Footer Social Icons-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <img src="img/Logo Oficial.png" style="width: 230px; left: 5px; position: relative; top: -5px;">
+                            <p id="Tpie1">
+                            <br>
+                            Agencia de diseño, desarrollo
+                            <br />
+                            y programación web
+                            <br>
+                            <br>
+                            ¡Siguenos!
+                        </p>
+                        <br>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://es-la.facebook.com/"><i class="fab fa-fw fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a>    
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/?lang=es"><i class="fab fa-fw fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://mx.linkedin.com/"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                    </div>
+                    <!-- Footer About Text-->
+                    <div class="col-lg-4">
+                        <h2 class="text-uppercase mb-4" id="Tpie">Contacto</h2>
+                        <p id="Tpie1">
+                            ejemplo@gmail.com
+                            <br />
+                            +52 2221524875
+                            <br>
+                            CDMX, COL. Benito Juárez
+                            <br>
+                            90700, Ciudad de México
+                        </p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        <!--</footer>
+         Copyright Section-->
+        <div class="copyright">
+            <div class="container"><small>Copyright© 2023 IDEA SOLUTIONS | todos los derechos reservados</small></div>
+        </div>
 </body>
 </html>
