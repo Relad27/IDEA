b970af67003d03fef608f0e9a55643a6de4f5af9
dany1412 <104804662+dany1412@users.noreply.github.com>
2023-07-17 16:43:16 -0600

    Add files via upload

--- Services/insertar.php
+++ Services/insertar.php
@@ -1,12 +1,12 @@
 <?php
 include("conexion.php");
 if(isset($_POST['boton'])){
-	if(strlen($_POST['name'])>=1 && strlen($_POST['correo'])>=1 && strlen($_POST['telefono'])>=1 && strlen($_POST['comentario']>=1)){
-		$nombre = trim($_POST["nombre"]);
-		$correo = trim($_POST["correo"]);
-		$telefono = trim($_POST["telefono"]);
-		$comentario = trim($_POST["comentario"]);
-		$insr = "insert into fomr_info(Nombre,Correo,Telefono,Descripcion) values ('$nombre','$correo','$telefono','$comentario')";
+	if(strlen($_POST['nombre'])>=1 && strlen($_POST['correo'])>=1 && strlen($_POST['telefono'])>=1 && strlen($_POST['comentario'])>=1){
+		$nombre = trim($_POST['nombre']);
+		$correo = trim($_POST['correo']);
+		$telefono = trim($_POST['telefono']);
+		$comentario = trim($_POST['comentario']);
+		$insr = "INSERT INTO fomr_info(Nombre,Correo,Telefono,Descripcion) values ('$nombre','$correo','$telefono','$comentario')";
 		$res = mysqli_query($conex,$insr);
         if ($res) {
             ?>
